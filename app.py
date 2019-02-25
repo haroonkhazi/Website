b180445938274149ae80fe1b8c0ee442769840ee
Haroon Khazi <haroonkhazi@gmail.com>
2019-02-25 00:04:29 -0500

    Updated templates and added picture

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def main_page():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
