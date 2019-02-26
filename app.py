99bde4f0a2c638fe8476ad44bde31d961424d038
Haroon Khazi <haroonkhazi@gmail.com>
2019-02-26 16:45:55 -0500

    adding home and more

--- app.py
+++ app.py
@@ -3,7 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def main_page():
+    return render_template('resume.html')
+
+@app.route("/home")
+def main_page():
     return render_template('home.html')
+
+@app.route("/resume")
+def resume():
+    return render_template('resume.html')
+
+@app.route("/projects")
+def resume():
+    return render_template('projects.html')
+
+@app.route("/wip")
+def resume():
+    return render_template('wip.html')
+
+@app.route("/pictures")
+def resume():
+    return render_template('pictures.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
